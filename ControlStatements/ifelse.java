3d0a7b81873ca5da436198b48714919ed0fb17cc
Kamal-Tenneti <kamalsekhartenneti@gmail.com>
2024-12-23 17:40:58 +0000

    ControlStatements

---
+++ ControlStatements/ifelse.java
@@ -0,0 +1,12 @@
+package ControlStatements;
+
+public class ifelse {
+	public static void main(String[]args) {
+	boolean isAuthenticated = true;
+	
+	if(isAuthenticated)
+		System.out.println("LoggedIn");
+	else
+		System.out.println("Not LoggedIn");
+	}
+}
