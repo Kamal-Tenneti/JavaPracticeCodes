d2b6a56a60df1befb4677ad50c95f273b49d3fbe
Kamal-Tenneti <kamalsekhartenneti@gmail.com>
2024-12-26 18:02:48 +0000

    Operators

---
+++ Operators/Operational.java
@@ -0,0 +1,10 @@
+package Operators;
+
+public class Operational{
+    public static void main(String[] args) {
+        int a=50, b=100, c=30;
+
+        int result=a + b+ c/10;
+        System.out.println(result);
+    }
+}
