abe9d7afbededd36233d1ac2fed4153317770849
Kamal-Tenneti <kamalsekhartenneti@gmail.com>
2024-12-29 10:26:09 +0000

    LoopConstructs

---
+++ LoopConstructs/forloop.java
@@ -0,0 +1,10 @@
+package LoopConstructs;
+
+public class forloop {
+    public static void main(String[] args) {
+        int initialValue = 1011;
+        for(int i = 0; i <= 100000; i++) {
+            System.out.print(initialValue);
+        }
+    }
+}
